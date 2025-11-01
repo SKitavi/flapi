b4a7469a5327335f803dbe0ca2baff426102a154
Sharon Kitavi <kitavisharon@gmail.com>
2025-11-01 05:39:55 +0300

    Fix syntax error in lerpColor function

--- src/game/GameEngine.js
+++ src/game/GameEngine.js
@@ -379,7 +379,7 @@ define([
       return [
         Math.floor(c1[0] + (c2[0] - c1[0]) * t),
         Math.floor(c1[1] + (c2[1] - c1[1]) * t),
-        Math.floor(c1[2] + (c2[2]) - c1[2]) * t)
+        Math.floor(c1[2] + (c2[2] - c1[2]) * t)
       ];
     },
     drawHeart: function(ctx, heart){
